28d1f041a5e57c028e9551b74f8ec35ea1de76eb
h_hayakawa <h_hayakawa@LAPTOP-5KH01P43>
2026-04-14 15:13:43 +0900

    全部未完成です前のstudentのを改変しながら作ってます

---
+++ src/main/java/bean/Subject.java
@@ -0,0 +1,38 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+
+	/**
+	 * 科目コード：String
+	 */
+	private String cd;
+
+	/**
+	 * 科目名：String
+	 */
+	private String name;
+
+
+
+	/**
+	 * ゲッタ・セッタ
+	 */
+	public String getCd() {
+		return cd;
+	}
+
+	public void setCd(String cd) {
+		this.cd = cd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
