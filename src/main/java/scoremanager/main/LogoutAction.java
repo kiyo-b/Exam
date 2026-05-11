1f2d29d983dff8b7b91cff6ef4c24eb305ebcf88
h_kiyokawa <h_kiyokawa@LAPTOP-222LUA17>
2026-05-11 10:23:59 +0900

    ログアウト完成

---
+++ src/main/java/scoremanager/main/LogoutAction.java
@@ -0,0 +1,21 @@
+package scoremanager.main;
+ 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+ 
+public class LogoutAction extends Action {
+	public void execute(
+			HttpServletRequest request, HttpServletResponse response
+	)throws Exception{
+		HttpSession session=request.getSession();
+		if (session.getAttribute("user")!=null) {
+			session.removeAttribute("user");
+		    request.getRequestDispatcher("/scoremanager/main/logout.jsp").forward(request, response);
+
+		}
+
+	}
+
+}
